639b535361786ae65f7b4542500697530946dbbd
tVv2Gk <tVv2Gk@cyber-dojo.org>
2022-09-08 18:52:18 +0000

    25 ran tests, no prediction, got green

--- files/hiker.js
+++ files/hiker.js
@@ -4,7 +4,7 @@ function closestToZero(listOfNumbers) {
   const positiveNumberArray = listOfNumbers.map(number => Math.abs(number));
   
   const minValueWithoutSign = Math.min(...positiveNumberArray);
-  const originalValue = listOfNumbers[positiveNumberArray.indexOf(minValue)];
+  const originalValue = listOfNumbers[positiveNumberArray.indexOf(minValueWithoutSign)];
   
   if(originalValue < 0 && listOfNumbers.indexOf(Math.abs(originalValue))>0) {
    return Math.abs(originalValue); 
