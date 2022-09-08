5f722ba75891ba15029998b4e48898b8e02dec98
tVv2Gk <tVv2Gk@cyber-dojo.org>
2022-09-08 18:21:25 +0000

    7 ran tests, no prediction, got red

--- files/hiker.test.js
+++ files/hiker.test.js
@@ -3,8 +3,12 @@
 const closestToZero = require('./hiker');
 
 describe('Testing the close to zero function', () => {
-  it('get the closest value for positive value', () => {
+  it('get the closest value for 2 positive value', () => {
     
     expect(closestToZero([1,2])).toEqual(1);
+  });
+  it('get the closest value for 3 positive value', () => {
+    
+    expect(closestToZero([7,2,9])).toEqual(2);
   });
 });
